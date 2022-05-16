ce59bea55404433a1f1c53a7bf62b97fd0f16837
lixiangyu-pos <58729399+lixiangyu-pos@users.noreply.github.com>
2022-05-16 16:07:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,13 +18,13 @@
 # Add a feed source
 
 ### 获取额外的 LuCI 应用、主题和依赖 ###
-git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-git clone https://github.com/281677160/luci-app-autoupdate.git package/luci-app-autoupdate
-git clone -b master --depth 1 https://github.com/kiddin9/luci-theme-edge package/new/luci-theme-edge
-svn export https://github.com/QiuSimons/openwrt-mos/trunk/mosdns package/new/mosdns
-svn export https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/new/luci-app-mosdns
-git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-git clone https://github.com/linkease/istore.git package/luci-app-store
+#git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+#git clone https://github.com/281677160/luci-app-autoupdate.git package/luci-app-autoupdate
+#git clone -b master --depth 1 https://github.com/kiddin9/luci-theme-edge package/new/luci-theme-edge
+#svn export https://github.com/QiuSimons/openwrt-mos/trunk/mosdns package/new/mosdns
+#svn export https://github.com/QiuSimons/openwrt-mos/trunk/luci-app-mosdns package/new/luci-app-mosdns
+#git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
+#git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+#git clone https://github.com/linkease/istore.git package/luci-app-store
